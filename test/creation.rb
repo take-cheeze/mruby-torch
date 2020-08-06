124bda393ad66b62e15d7c48d529e141670922a8
take-cheeze <takechi101010@gmail.com>
2020-08-06 10:31:40 +0900

    Add rand test

--- test/creation.rb
+++ test/creation.rb
@@ -2,3 +2,8 @@ assert 'Torch.ones' do
   t = Torch.ones([10, 10])
   assert_equal "CPUFloatType", t.to_s
 end
+
+assert 'Torch.rand' do
+  t = Torch.rand([10, 10])
+  assert_equal "CPUFloatType", t.to_s
+end
