64815216e5c035ee005854215b4edd4668c2ab28
take-cheeze <takechi101010@gmail.com>
2020-08-10 12:37:46 +0900

    Enable sanitizer

--- .github/workflows/custom_config.rb
+++ .github/workflows/custom_config.rb
@@ -2,5 +2,10 @@ MRuby::Build.new do |conf|
   toolchain :gcc
   enable_debug
   enable_test
+
+  conf.cc.flags << '-fsanitize=address,leak,undefined'
+  conf.cxx.flags << '-fsanitize=address,leak,undefined'
+  conf.linker.flags << '-fsanitize=address,leak,undefined'
+
   conf.gem "#{MRUBY_ROOT}/.."
 end
