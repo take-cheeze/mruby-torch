174aab2944a5bb2373ff0e0b008f84d51b41f434
take-cheeze <takechi101010@gmail.com>
2020-08-16 16:31:52 +0900

    Fix duplicate of / operator

--- mrblib/tensor.rb
+++ mrblib/tensor.rb
@@ -4,7 +4,7 @@ module Torch
     def - rhs; self.sub rhs end
     def * rhs; self.mul rhs end
     def / rhs; self.div rhs end
-    def / rhs; self.div rhs end
+    def % rhs; self.remainder rhs end
 
     alias shape sizes
   end
